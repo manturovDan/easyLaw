b8a841559ae1cd55350ee21ddcdf7c34e7a1f9fe
manturovDan <manturov.dan@mail.ru>
2020-05-29 22:56:11 +0300

    run via init

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,10 @@
 from flask import Flask
+from src.auth.auth import auth
 
 
 def create_app():
+
     app = Flask(__name__)
-
-    print(123)
-    # blueprint for auth routes in our app
-    from src.auth.auth import login as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='auth')
-
-    # blueprint for non-auth parts of app
-    #from .main import main as main_blueprint
-    #app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth, url_prefix='/auth')
 
     return app
